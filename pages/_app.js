9c13902ff6813a02296ab65c760bdb55335d5355
borisgarkov <boris.garkov@abv.bg>
2023-01-13 11:13:08 +0200

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import '../styles/globals.css'
+// import '../styles/globals.css'
+import { Cormorant_Garamond } from '@next/font/google';
+
+const mainFontStyle = Cormorant_Garamond({
+  weight: ['300', '400', '500', '600', '700'],
+  subsets: ['latin']
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <main className={mainFontStyle.className}>
+    <Component {...pageProps} />
+  </main>
 }
+
+export { mainFontStyle };

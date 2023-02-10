7be588072713ffb42b8032d374b7c423bdb89ba0
borisgarkov <boris.garkov@abv.bg>
2023-02-10 16:50:02 +0200

    updated divider

--- components/Home/DividerSection.js
+++ components/Home/DividerSection.js
@@ -1,8 +1,7 @@
-import { Divider } from "@mui/material";
 import styles from './divider-style.module.css';
 
 export default function DividerSection() {
     return (
-        <Divider className={styles.divider} />
+        <hr className={styles.divider}></hr>
     )
 };

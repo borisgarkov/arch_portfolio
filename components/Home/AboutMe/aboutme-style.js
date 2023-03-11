5e3bff4c2ef1b38411f1c9b5e650338afd28686e
borisgarkov <boris.garkov@abv.bg>
2023-03-11 16:52:33 +0200

    adding server rendered components

--- components/Home/AboutMe/aboutme-style.js
+++ components/Home/AboutMe/aboutme-style.js
@@ -1,4 +1,4 @@
-import styled from "@emotion/styled";
+import styled from "styled-components";
 
 const MainContainer = styled.div`
     margin-top: 40px;
--- next.config.js
+++ next.config.js
@@ -1,17 +1,10 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
 	reactStrictMode: true,
-	images: {
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: 'source.unsplash.com',
-				port: '',
-				pathname: '/random/**',
-			},
-		],
+	compiler: {
+		styledComponents: true,
 	},
 }
 
 module.exports = nextConfig
-//  https://source.unsplash.com/random/tree/

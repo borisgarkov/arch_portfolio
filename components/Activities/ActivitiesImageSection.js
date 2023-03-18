e39a05d98cf18c621cd89ee50ed9f94707c8bf48
borisgarkov <boris.garkov@abv.bg>
2023-03-18 15:28:03 +0200

    updating activities page

--- components/Activities/ActivitiesImageSection.js
+++ components/Activities/ActivitiesImageSection.js
@@ -2,11 +2,13 @@ import Box from "@mui/material/Box";
 import Image from "next/image";
 import useScreenResolution from "../../hooks/useScreenResolution";
 import IconImageFrame from "./IconImageFrame";
-// import backgroundImage from '../../public/activities/background.jpg';
-// import backgroundReverseImage from '../../public/activities/background-reverse.jpg';
 
 export default function ActivitiesImageSection({ activity }) {
     const isMobile = useScreenResolution('lg');
+
+    const imageSrc = isMobile || !activity.reverse
+        ? '/activities/background.jpg'
+        : '/activities/background-reverse.jpg'
 
     return (
         <Box sx={{
@@ -14,24 +16,12 @@ export default function ActivitiesImageSection({ activity }) {
             height: '180px',
             position: 'relative',
         }}>
-            {/* <Image
-                src={
-                    isMobile
-                        ? backgroundImage
-                        : activity.reverse
-                            ? backgroundReverseImage
-                            : backgroundImage
-                }
-                alt='background-image'
-                placeholder='blur'
+            <Image
                 priority
-                style={{
-                    width: '100%',
-                    height: '100%',
-                    position: 'absolute',
-                    objectFit: 'cover',
-                }}
-            /> */}
+                src={imageSrc}
+                alt='background-image'
+                fill
+            />
             <IconImageFrame activity={activity} />
         </Box >
     )
--- components/Activities/IconImageFrame.js
+++ components/Activities/IconImageFrame.js
@@ -2,26 +2,24 @@ import Box from "@mui/material/Box";
 import styles from './styles.module.css';
 import Image from "next/image";
 import frame from '../../public/activities/frame.png';
+import useScreenResolution from "../../hooks/useScreenResolution";
 
 export default function IconImageFrame({ activity }) {
+    const isMobile = useScreenResolution('lg');
+
     return (
         <Box sx={{
             opacity: 0.7,
             width: '170px',
             height: '170px',
             position: 'relative',
+            marginLeft: !isMobile && !activity.reverse ? 'auto' : null
         }}>
             <Image
                 src={frame}
                 alt='frame'
-                placeholder='blur'
                 priority
-                style={{
-                    width: '100%',
-                    height: '100%',
-                    position: 'absolute',
-                    objectFit: 'cover',
-                }}
+                fill
             />
             <Image
                 src={activity.image}
